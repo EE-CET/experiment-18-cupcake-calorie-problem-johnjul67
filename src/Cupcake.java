a218cd26ed2a626a0a06c5ee2e7419e6c0c0b977
John <tve24el033@cet.ac.in>
2026-01-30 02:00:10 +0530

    commit2

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,29 +1,21 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cupcake 
 {
-        public static void main(String[] args)
+        public static void main(String[] args) 
         {
                 Scanner sc = new Scanner(System.in);
                 int n = sc.nextInt();
-                int arr[] = new int[n];
-                for (int i=0;i<n;i++)
+                int[] arr = new int[n];
+                for (int i = 0; i < n; i++)
                         arr[i] = sc.nextInt();
-                for (int i=0;i<n-1;i++)
-                {
-                        for (int j=0;j<n-i-1;j++)
-                        {
-                                if (arr[j]>arr[j+1])
-                                {
-                                        int temp= arr[j];
-                                        arr[j]=arr[j+1];
-                                        arr[j+1]=temp;
-                                }       
-                        }
-                }
+
+                Arrays.sort(arr);
                 long miles = 0;
-                for (int i = 0; i < n; i++) 
-                        miles += arr[i] * (1L << i); 
+                for (int i = 0; i < n; i++)
+                        miles += arr[n - 1 - i] * (1L << i);
+
                 System.out.println(miles);
-        }   
+    }   
 }
